de65818d41bee4242abf1002bb84e4497db82b63
jpsjogren <52937165+jpsjogren@users.noreply.github.com>
2020-05-13 14:33:24 +0200

    Dices

--- KniziaYatzy/src/program/Main.java
+++ KniziaYatzy/src/program/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hallå!");
-		System.out.println("Kan du se detta???");
+		
+		//Just for testing prurpouse
+		Dices dices = new Dices();
+		dices.rollDice();
 		
 	}
 

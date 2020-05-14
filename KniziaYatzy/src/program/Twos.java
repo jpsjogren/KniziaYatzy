58257d43a873470c7619f4a569d33accfe3211c9
fleur-power <58519073+fleur-power@users.noreply.github.com>
2020-05-14 12:31:50 +0200

    Create Twos.java

---
+++ KniziaYatzy/src/program/Twos.java
@@ -0,0 +1,10 @@
+// Upper Section Twos Rule (#2) implementation
+
+package program;
+
+class Twos extends AnyStorage implements Rule {
+
+  public int calculate(Rule[] protocol, RollDice result) {
+    return 1;
+  }
+}

afa450e532e4a9fccfa63cec9a3c46c770ebf364
fleur-power <58519073+fleur-power@users.noreply.github.com>
2020-05-15 12:14:33 +0200

    API refinement

--- KniziaYatzy/src/program/Protocol.java
+++ KniziaYatzy/src/program/Protocol.java
@@ -5,28 +5,28 @@ package program;
 //for(int rulenum=0; rulenum < p.number_of_rules; ++rulenum) {
 //    int[] dice_contents; // int[5]
 //    int score;
-//    if (p.rules[rulenum].cast_is_needed()) {
+//    if (p.cast_is_needed(rulenum)) {
 //        // as much as you wish (1-3 times)
 //	  do {
 //	      // put relevant data into dice_contents...
 //	      dice_contents = new WhateverProducesThis(whatever_data);
 //	      // calculate score of dices (or/and of the existing entries in the protocol)
-//	      score = p.rules[rulenum].calculate(p, dice_contents);
+//	      score = p.calculate(rulenum, dice_contents);
 //	      // act (e.g. show/ask/...) on the score
 //	  } while (the_player_and_the_rules_allow);
 //	  // store the score into the protocol
-//	  p.rules[rulenum].store(score);
+//	  p.store(rulenum, score);
 //    } else {
-//	  score = p.rules[rulenum].calculate(p, null);
+//	  score = p.calculate(rulenum, null);
 //    }
 //}
 //----------------------------
 //// when needed, read score from an earlier rule:
-//int earlier_score = p.rules[earlier_rulenum].peek();
+//int earlier_score = p.peek(earlier_rulenum);
 //----------------------------
 //// metadata:
-//String rule_name = p.rules[rulenum].rule_name();
-//String rule_description = p.rules[rulenum].rule_description();
+//String rule_name = p.rule_name(rulenum);
+//String rule_description = p.rule_description(rulenum);
 //----------------------------
 
 public class Protocol {
@@ -39,7 +39,7 @@ public class Protocol {
   final int number_of_rules = 1;
   
   String player_name;
-  
+ 
   Protocol(String player) throws Exception {
     player_name = player;
     rules = new Rule[number_of_rules];
@@ -51,5 +51,24 @@ public class Protocol {
     if (i != number_of_rules) {
       throw new Exception("Internt fel BADRULECOUNT"); // "Internal error - wrong number of rules"
     }
+  }
+  
+  boolean cast_is_needed(int rulenum) {
+      return rules[rulenum].cast_is_needed();
+  }
+  void store(int rulenum, int score) throws Exception {
+      rules[rulenum].store(score);
+  }
+  int peek(int rulenum) throws Exception {
+      return rules[rulenum].peek();
+  }
+  int calculate(int rulenum, int[] dices) {
+      return rules[rulenum].calculate(this, dices);
+  }
+  String rule_name(int rulenum) {
+      return rules[rulenum].rule_name();
+  }
+  String rule_description(int rulenum) {
+      return rules[rulenum].rule_description();
   }
 }

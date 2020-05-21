10cfad805e52cb2f3caf7117618a45571348d228
fleur-power <58519073+fleur-power@users.noreply.github.com>
2020-05-21 14:10:07 +0200

    add TestRules and TestRunner

---
+++ KniziaYatzy/src/test/TestRunner.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunner {
+   public static void main(String[] args) {
+      Result result = JUnitCore.runClasses(TestRules.class,
+                                           TestDice.class,
+                                           TestHold.class);
+
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure);
+      }
+
+      if (result.wasSuccessful()) System.out.println("Yeah baby!");
+      else                        System.out.println("Errors have been detected");
+   }
+}

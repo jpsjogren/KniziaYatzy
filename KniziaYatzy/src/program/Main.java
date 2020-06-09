9810897e49838c16801328f6d029c51a90b87262
jpsjogren <52937165+jpsjogren@users.noreply.github.com>
2020-06-09 16:20:12 +0200

    .

--- KniziaYatzy/src/program/Main.java
+++ KniziaYatzy/src/program/Main.java
@@ -1,10 +1,13 @@
 package program;
 
+import highscore.*;
+
 public class Main {
 	static Menu menu = new Menu();
 	public static void main(String[] args) throws Exception {
 		System.out.println("Välkommen till Yatzy!");
 		System.out.println("=====================");
+		//HighScore.showTopTenResults();
 		menu.startMenu();
 	}
 }

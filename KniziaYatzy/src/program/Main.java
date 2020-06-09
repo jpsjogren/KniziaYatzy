9d3c0d6fec5ff5764ef17e7c2c3598235cea7ae1
jpsjogren <52937165+jpsjogren@users.noreply.github.com>
2020-06-09 15:46:50 +0200

    Update Main.java

--- KniziaYatzy/src/program/Main.java
+++ KniziaYatzy/src/program/Main.java
@@ -7,7 +7,6 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Välkommen till Yatzy!");
 		System.out.println("=====================");
-		HighScore.showTopTenResults();
 		menu.startMenu();
 	}
 }

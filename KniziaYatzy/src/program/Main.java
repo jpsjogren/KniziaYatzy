69c92c4ee5b0ae9ee2e14b68722a7244ed4b5742
fleur-power <58519073+fleur-power@users.noreply.github.com>
2020-06-16 15:32:28 +0200

    fixed formatting

--- KniziaYatzy/src/program/Main.java
+++ KniziaYatzy/src/program/Main.java
@@ -1,12 +1,10 @@
 package program;
 
-import highscore.HighScore;
-
 public class Main {
 	static Menu menu = new Menu();
 	public static void main(String[] args) throws Exception {
 		System.out.println("Välkommen till Maxiyatzy!");
-		System.out.println("=====================");
+		System.out.println("=========================");
 		menu.startMenu();
 	}
 }
